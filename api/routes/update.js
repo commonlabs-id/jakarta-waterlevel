52f194e6bf01fde01d00e64bf33b3295ed6f7d16
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2019-05-15 16:51:41 +0700

    fix(api): Fix environment var name

--- api/routes/update.js
+++ api/routes/update.js
@@ -4,9 +4,7 @@ const Queue = require("bull");
 const { getDateString } = require("../utils/time");
 const IS_DEV = require("../utils/is-dev");
 
-const REDIS_URL = IS_DEV
-  ? "redis://127.0.0.1:6379"
-  : process.env.JW_F_REDIS_URL;
+const REDIS_URL = IS_DEV ? "redis://127.0.0.1:6379" : process.env.JW_REDIS_URL;
 
 let workQueue;
 
