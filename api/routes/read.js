2f031efac31cd8c1abd2179d9c0164aa05e59a1c
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2019-05-18 10:05:49 +0700

    feat(api:read): Handle edge case where today's data isn't ready yet

--- api/routes/read.js
+++ api/routes/read.js
@@ -4,10 +4,18 @@ const { readLevelsOnDate } = require("jw-database");
 const { getDateString } = require("../utils/time");
 
 async function handler(req, res) {
+  const date = new Date();
   try {
-    const now = getDateString(new Date());
-    const data = await readLevelsOnDate(now);
-    send(res, 200, data);
+    const dateString = getDateString(date);
+    const data = await readLevelsOnDate(dateString);
+    if (data) {
+      send(res, 200, data);
+    } else {
+      console.log(dateString, `isn't ready yet, using yesterday data.`);
+      const yesterday = getDateString(date, 1);
+      const yData = await readLevelsOnDate(yesterday);
+      send(res, 200, yData);
+    }
   } catch (e) {
     console.error(e);
     send(res, 500, e.message);
